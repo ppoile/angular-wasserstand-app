ea0192e3e6015e8198ca834c7cf937e993df3632
Andi Meier <andi@daemeier-clan.ch>
2018-11-21 22:06:48 +0100

    app: add measurement service

---
+++ src/app/measurement.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { delay, mapTo } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MeasurementService {
+  url = "https://www.hydrodaten.admin.ch/de/2082.html";
+
+  constructor(private http: HttpClient) { }
+
+  getObservable() {
+    var observable = this.http.get(this.url, {responseType: 'text'})
+    observable.subscribe(
+      (data) => { console.log(`data: ${data}`); },
+      error => { console.log(`error: ${error}`); },
+    );
+    return this.getObservable2();
+  }
+
+  getObservable2() {
+    const dummy = of(null);
+    return dummy.pipe(
+      mapTo(434.77),
+      delay(1000));
+  }
+}
