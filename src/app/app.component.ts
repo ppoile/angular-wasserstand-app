6a8479a84c0cae36814e78a95b5ba95e5dd9d208
Andi Meier <andi@daemeier-clan.ch>
2018-11-21 22:12:05 +0100

    app.component: use measurement service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { MeasurementService } from './measurement.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wasserstand-app';
+  measurement = "(unknown)";
+
+  constructor(private measurementService: MeasurementService) {
+    this.subscribeMeasurement();
+    console.log(`measurement: ${this.measurement}`);
+  }
+
+  subscribeMeasurement(): void {
+    const observable = this.measurementService.getObservable();
+    observable.subscribe(measurement => {
+      console.log(`measurement: ${measurement}`);
+      this.measurement = measurement.toString();
+    });
+  }
 }
